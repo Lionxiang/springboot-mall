2e68d2b922deac5b55cc62beccf382cf11ea833e
Lionxiang <j7415817@gmail.com>
2024-01-28 16:21:54 +0800

    product-function - update function

--- src/main/java/com/songjimmy/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/songjimmy/springbootmall/dao/impl/ProductDaoImpl.java
@@ -43,7 +43,7 @@ public class ProductDaoImpl implements ProductDao {
     public Integer createProduct(ProductRequest productRequest) {
         String sql = "insert into product(product_name, category, image_url, price, stock, description, " +
                 "created_date, last_modified_date) values (:productName, :category, :imageUrl, :price," +
-                ":stock, :description, :createdDate, :lateModifiedDate)";
+                ":stock, :description, :createdDate, :lastModifiedDate)";
 
         Map<String, Object> map = new HashMap<>();
         map.put("productName", productRequest.getProductName());
@@ -55,7 +55,7 @@ public class ProductDaoImpl implements ProductDao {
 
         Date now = new Date();
         map.put("createdDate", now);
-        map.put("lateModifiedDate", now);
+        map.put("lastModifiedDate", now);
 
         KeyHolder keyHolder = new GeneratedKeyHolder();
         namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource(map), keyHolder);
@@ -63,5 +63,25 @@ public class ProductDaoImpl implements ProductDao {
         int productId = keyHolder.getKey().intValue();
 
         return productId;
+    }
+
+    @Override
+    public void updateProduct(Integer productId, ProductRequest productRequest) {
+        String sql = "update product set product_name = :productName, category = :category, " +
+                "image_url = :imageUrl, price = :price, stock = :stock, description = :description, " +
+                "last_modified_date = :lastModifiedDate where product_id = :productId";
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("productId", productId);
+        map.put("productName", productRequest.getProductName());
+        map.put("category", productRequest.getCategory().toString());
+        map.put("imageUrl", productRequest.getImageUrl());
+        map.put("price", productRequest.getPrice());
+        map.put("stock", productRequest.getStock());
+        map.put("description", productRequest.getDescription());
+
+        map.put("lastModifiedDate", new Date());
+
+        namedParameterJdbcTemplate.update(sql, map);
     }
 }
