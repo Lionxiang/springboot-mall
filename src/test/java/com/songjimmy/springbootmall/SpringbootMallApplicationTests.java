e0f2b199234f94092ddfba5dd5dc220fbf671549
Lionxiang <j7415817@gmail.com>
2024-01-28 11:53:26 +0800

    initial again

---
+++ src/test/java/com/songjimmy/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songjimmy.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
